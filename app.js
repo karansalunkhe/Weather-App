76f68be80e915f8689200aa58e6a961fda54ae05
karan <karansalunkhe88@gmail.com>
2021-05-18 15:02:13 +0530

    new modified

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.post("/", function(req, res){
             const icon= weatherData.weather[0].icon;
             const imageUrl= "http://openweathermap.org/img/wn/"+ icon + "@2x.png";
             res.write("<p> The weather is currently"+ weatherDescription +"</p>");
-            res.write("<h1> The temp in "+ query+" is "+ temp + "degrees celcius </h1>");
+            res.write("<h1> The temp in "+ query+" is "+ temp + "degree F </h1>");
             res.write("<img src=" + imageUrl + ">");
             res.send();
         })
